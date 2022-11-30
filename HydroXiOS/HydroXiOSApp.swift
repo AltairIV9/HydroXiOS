9fa9a25af7bcdd0df1c5ce086615ba7f0779576d
AltairIV9 <walterhugo_souza@hotmail.com>
2022-11-30 07:58:19 -0300

    Initial Commit

---
+++ HydroXiOS/HydroXiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydroXiOSApp.swift
+//  HydroXiOS
+//
+//  Created by Walter Hugo on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HydroXiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
