56a19f2083f5f1e8b73b609e591685d984b03426
AltairIV9 <walterhugo_souza@hotmail.com>
2022-11-30 16:13:53 -0300

    Initial Commit

--- HydroXiOS/ContentView.swift
+++ HydroXiOS/ContentView.swift
@@ -6,14 +6,35 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct ContentView: View {
+    
+    @State var quantidadeTotal: String = "0/0 ml"
+    @State var tamanhoCopo: String = "0 ml"
+        
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            VStack(spacing: 10){
+                Text(quantidadeTotal)
+                    .font(.system(size:33))
+                
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text(tamanhoCopo)
+                        .font(.system(size: 24))
+                        .foregroundColor(.black)
+                })
+                
+            }
+            Button(action: {
+                
+            }, label: {
+                Text("Beber")
+                    .font(.system(size:24))
+            })
         }
         .padding()
     }
